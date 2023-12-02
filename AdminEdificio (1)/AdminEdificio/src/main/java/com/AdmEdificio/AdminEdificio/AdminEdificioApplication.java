ff1b4d7777966801a735f3d117fc0925e87566de
Felipe Carrasco Norero <felipe.carrasco.n@usach.cl>
2023-12-02 17:54:18 -0300

    git pa todo chile

---
+++ AdminEdificio (1)/AdminEdificio/src/main/java/com/AdmEdificio/AdminEdificio/AdminEdificioApplication.java
@@ -0,0 +1,13 @@
+package com.AdmEdificio.AdminEdificio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminEdificioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminEdificioApplication.class, args);
+	}
+
+}
---
+++ AdminEdificio (1)/AdminEdificio/src/test/java/com/AdmEdificio/AdminEdificio/AdminEdificioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdmEdificio.AdminEdificio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminEdificioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

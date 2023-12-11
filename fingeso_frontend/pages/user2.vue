f48ff3b0b5c4657b36a9d94afed25ebeeb722306
gonzaloArevalo <gonzalo.arevalo@usach.cl>
2023-12-11 16:49:10 -0300

    cambios a residente

--- fingeso_frontend/pages/user2.vue
+++ fingeso_frontend/pages/user2.vue
@@ -1,5 +1,6 @@
 <script>
 export default {
+  layout: 'layout2',
   name: 'user2',
   data() {
     return {
@@ -20,10 +21,23 @@ export default {
 
 
 <template>
-
-
-    <h1 v-if="user"> HOLA {{ user }}</h1>
-
-
-    
+<div>
+    <h1 v-if="user" class = "user-name"> Bienvenido {{ this.user.nombre }}</h1>
+  <p>
+    Usted acaba de iniciar la aplicación, Esta diseñada con el fin de favorecer el
+    uso simple de las cuentas de la comunidad establecida para que tenga una experiencia
+    más fluida, dentro de la barra de tareas encontrara las opciones para salir de esta
+    como para ver sus deudas pendientes y efectuar los pagos antes de que acumulen intereses,
+    finalmente recuerde siempre tener un buen día.
+    Gracias por su atención.
+  </p>
+</div>
 </template>
+
+<style scoped>
+.user-name {
+  font-weight: bold;
+  color: #3498db;
+  margin-bottom: 45px;
+}
+</style>
